f503e0f8df1387b6761fdd91c54c5625b660e25a
lijiangming <lijm@missfresh.cn>
2019-09-20 17:24:07 +0800

    add dubbo demo

---
+++ dubbo-provider/src/main/java/top/ljming/dubboprovider/service/DemoServiceImpl.java
@@ -0,0 +1,9 @@
+package top.ljming.dubboprovider.service;
+
+/**
+ * 描述类的功能.
+ *
+ * @author lijm
+ */
+public class DemoServiceImpl {
+}
---
+++ learning-sdk/src/main/java/top/ljming/learning/dubboservice/DemoService.java
@@ -0,0 +1,11 @@
+package top.ljming.learning.dubboservice;
+
+/**
+ * 描述类的功能.
+ *
+ * @author lijm
+ */
+public interface DemoService {
+
+    public String demo(String name);
+}
